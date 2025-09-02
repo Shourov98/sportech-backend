6ba0ee6febd911bbd87083c5cc3ec013e13f9079
shourov98 <mdmahbub0412@gmail.com>
2025-09-03 03:55:41 +0600

    create: Service model

---
+++ models/Service.js
@@ -0,0 +1,24 @@
+const mongoose2 = require("mongoose");
+
+const ServiceSchema = new mongoose2.Schema(
+  {
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    title: { type: String, required: true },
+    subtitle: String,
+    shortDesc: String,
+    description: String,
+    bannerImage: String, // /service/... or URL
+    rightImage: String, // /service/[slug]-image.png or URL
+    published: { type: Boolean, default: true },
+    order: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose2.model("Service", ServiceSchema);
